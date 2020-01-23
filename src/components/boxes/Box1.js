0b546860968b25956f3f8657f7d66c4a3b0526d7
Adam Clarkson <atclarkson@gmail.com>
2020-01-22 22:06:18 -0500

    change my name to display via state in lieu of hardcoding

--- src/components/boxes/Box1.js
+++ src/components/boxes/Box1.js
@@ -2,10 +2,13 @@ import React from "react";
 import Alert from 'react-bootstrap/Alert'
 
 function Box1() {
+
+    const [name] = React.useState("Adam Clarkson");
+
     return (
         <div className="box1">
             <Alert variant='primary'>
-                <h1>Adam Clarkson</h1>
+                <h1>{name}</h1>
             </Alert>
         </div>
     );
